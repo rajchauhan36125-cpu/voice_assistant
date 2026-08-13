476e418a546c4d2ff3ebc581cd85ae9a329cf69f
rajchauhan36125-cpu <rajchauhan36125@gmail.com>
2026-08-13 15:36:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from kivy.uix.label import Label
 
 class KIApp(App):
     def build(self):
-        return Label(text='Hello K.I')
+        return Label(text="K.I", font_size="50sp")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     KIApp().run()
-    
