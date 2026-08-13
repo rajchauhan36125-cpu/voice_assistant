263f36fc17b803aa6aa1df574b13d380d7f7a5f2
rajchauhan36125-cpu <rajchauhan36125@gmail.com>
2026-08-13 13:21:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class VoiceAssistantApp(App):
+class KIApp(App):
     def build(self):
-        return Label(text='Namaste! Mera AI Assistant taiyar ho raha hai.')
+        return Label(text='Hello K.I')
 
 if __name__ == '__main__':
-    VoiceAssistantApp().run()
-
-# Trigger build
+    KIApp().run()
+    
